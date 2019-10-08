33aaa55006347c54af9b30d3a193b39b945f8bce
mc1998 <manuelcarrera.sbm@gmail.com>
2019-10-08 17:08:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -138,5 +138,5 @@ app.get('/api/suitswitch/recent', function(req, res){
 // app.listen(process.env.PORT || 3000, function(){
 // 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 // });
-app.listen(process.env.PORT || 5000)
-console.log('Server is running on port 5000...')
+app.listen(process.env.PORT || 3000)
+console.log('Server is running on port 3000...')
